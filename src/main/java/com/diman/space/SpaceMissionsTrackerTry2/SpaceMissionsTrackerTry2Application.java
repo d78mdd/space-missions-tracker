af51d1c3c82b02cc9e66e881bb944a45d8c08ebc
dkostadinov <d78mdd@gmail.com>
2024-06-29 20:32:06 +0300

    - remove TODO - fix testGetMissionsByDate

--- src/main/java/com/diman/space/SpaceMissionsTrackerTry2/SpaceMissionsTrackerTry2Application.java
+++ src/main/java/com/diman/space/SpaceMissionsTrackerTry2/SpaceMissionsTrackerTry2Application.java
@@ -19,7 +19,6 @@ public class SpaceMissionsTrackerTry2Application {
     TODO use swagger
     TODO use DTO and mapping
     TODO validations
-    TODO add variant using Gradle instead of Maven
     TODO add variant using yaml instead application properties
 
     TODO use more conscious way for mockMvc.perform(post("/api/missions")
