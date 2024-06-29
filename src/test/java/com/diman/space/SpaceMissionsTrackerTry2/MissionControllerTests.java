d5b42d2154d382321cf1e486b2cc9d132367ef22
dkostadinov <d78mdd@gmail.com>
2024-06-29 16:55:07 +0300

    rename test class

--- src/test/java/com/diman/space/SpaceMissionsTrackerTry2/MissionControllerTests.java
+++ src/test/java/com/diman/space/SpaceMissionsTrackerTry2/MissionControllerTests.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MissionController.class)
-class SpaceMissionsTrackerTry2ApplicationTests {
+class MissionControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
