bcdbca31e672c06e1c2257224187923ed3ad2344
dkostadinov <d78mdd@gmail.com>
2024-06-30 12:15:50 +0300

    - implement DAO tests insert(), deleteById()

--- src/test/java/com/diman/space/SpaceMissionsTrackerTry2/dao/MissionDaoTests.java
+++ src/test/java/com/diman/space/SpaceMissionsTrackerTry2/dao/MissionDaoTests.java
@@ -1,6 +1,7 @@
 package com.diman.space.SpaceMissionsTrackerTry2.dao;
 
 import com.diman.space.SpaceMissionsTrackerTry2.model.Mission;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,12 +10,16 @@ import java.time.LocalDate;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 public class MissionDaoTests {
 
     @Autowired
     private MissionDao dao;
+
+
+    // TODO shorten lines / extract test data for missions as beforeTest
 
 
     @Test
@@ -35,7 +40,7 @@ public class MissionDaoTests {
 
     @Test
     void findById() {
-        Mission mission = new Mission(1L, "Curiosity Rover", LocalDate.of(2011, 11, 26), "Ongoing", "Mars rover exploring the surface of Mars");
+        Mission mission = new Mission("Curiosity Rover", LocalDate.of(2011, 11, 26), "Ongoing", "Mars rover exploring the surface of Mars");
         dao.insert(mission);
 
         Mission result = dao.findById(1L).get();
@@ -46,10 +51,27 @@ public class MissionDaoTests {
 
     @Test
     void insert() {
+        Mission mission = new Mission("Hubble Space Telescope", LocalDate.of(1990, 4, 24), "Ongoing", "Orbiting telescope for astronomical observations");
+
+        dao.insert(mission);
+
+        Mission insertedMission = dao.findById(1L).get();
+        assertNotNull(insertedMission);
+        assertEquals(mission.getName(), insertedMission.getName());
     }
 
     @Test
     void deleteById() {
+        Mission mission = new Mission("ISS Expedition 1", LocalDate.of(2000, 10, 31), "Completed", "First long-duration stay on the International Space Station");
+        dao.insert(mission);
+        Mission resultBeforeDelete = dao.findById(1L).get();
+
+        dao.deleteById(1L);
+
+        Mission resultAfterDelete = dao.findById(1L).orElse(null);
+
+        Assertions.assertNotNull(resultBeforeDelete);
+        assertNull(resultAfterDelete);
     }
 
     @Test
