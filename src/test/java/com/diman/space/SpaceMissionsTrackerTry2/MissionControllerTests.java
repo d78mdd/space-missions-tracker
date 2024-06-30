41fa44448c038b257e8075a19c31c24f538da639
dkostadinov <d78mdd@gmail.com>
2024-06-30 17:30:09 +0300

    - add initial controller integration test testGetMissionsByDate

--- src/test/java/com/diman/space/SpaceMissionsTrackerTry2/MissionControllerTests.java
+++ src/test/java/com/diman/space/SpaceMissionsTrackerTry2/MissionControllerTests.java
@@ -1,11 +1,15 @@
 package com.diman.space.SpaceMissionsTrackerTry2;
 
+import com.diman.space.SpaceMissionsTrackerTry2.model.Mission;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -36,6 +40,19 @@ public class MissionControllerTests {
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].name", is("Rosetta")))
                 .andExpect(jsonPath("$[1].name", is("Luna 2")));
+    }
+
+    @Test
+    public void testGetMissionsByDate() throws Exception {
+        mockMvc.perform(post("/api/missions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\": \"Vostok 1\", \"launchDate\": \"1961-04-12\", \"status\":\"Completed\", \"description\":\"First human spaceflight, carried Yuri Gagarin into orbit.\"}"));
+
+        mockMvc.perform(get("/api/missions/search?date=1961-04-12")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is("Vostok 1")));
     }
 
 }
