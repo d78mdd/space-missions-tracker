528ac61e6b4e6e50d52ddd186c96f7ac685deee6
dkostadinov <d78mdd@gmail.com>
2024-06-30 12:03:33 +0300

    - implement DAO test findById()

--- src/test/java/com/diman/space/SpaceMissionsTrackerTry2/dao/MissionDaoTests.java
+++ src/test/java/com/diman/space/SpaceMissionsTrackerTry2/dao/MissionDaoTests.java
@@ -1,8 +1,14 @@
 package com.diman.space.SpaceMissionsTrackerTry2.dao;
 
+import com.diman.space.SpaceMissionsTrackerTry2.model.Mission;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class MissionDaoTests {
@@ -29,6 +35,13 @@ public class MissionDaoTests {
 
     @Test
     void findById() {
+        Mission mission = new Mission(1L, "Curiosity Rover", LocalDate.of(2011, 11, 26), "Ongoing", "Mars rover exploring the surface of Mars");
+        dao.insert(mission);
+
+        Mission result = dao.findById(1L).get();
+
+        assertNotNull(result);
+        assertEquals(mission.getName(), result.getName());
     }
 
     @Test
