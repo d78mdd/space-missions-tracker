b91f99cd78b25454da02aafbf0c33edb403170f9
dkostadinov <d78mdd@gmail.com>
2024-06-30 11:52:58 +0300

    - add frame for DAO tests

---
+++ src/test/java/com/diman/space/SpaceMissionsTrackerTry2/dao/MissionDaoTests.java
@@ -0,0 +1,45 @@
+package com.diman.space.SpaceMissionsTrackerTry2.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MissionDaoTests {
+
+    @Autowired
+    private MissionDao dao;
+
+
+    @Test
+    void findByNameContainingIgnoreCase() {
+    }
+
+    @Test
+    void findByExactName() {
+    }
+
+    @Test
+    void findByLaunchDate() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void update() {
+    }
+}
